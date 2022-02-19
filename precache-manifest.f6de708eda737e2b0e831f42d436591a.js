6828b16d677300ba3e8afee8874a048778901f71
Guntuboina Sankirtana <59257797+gvsankirtana@users.noreply.github.com>
2022-02-19 15:37:35 +0300

    Updates

---
+++ precache-manifest.f6de708eda737e2b0e831f42d436591a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae5cb991be4ce67fdb2a5ca215603af",
+    "url": "/Text-Modifier/index.html"
+  },
+  {
+    "revision": "c49c26a0ff6bd224ef74",
+    "url": "/Text-Modifier/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "1316a5a852c3d3b53dba",
+    "url": "/Text-Modifier/static/css/main.de07eba2.chunk.css"
+  },
+  {
+    "revision": "c49c26a0ff6bd224ef74",
+    "url": "/Text-Modifier/static/js/2.1dcc8719.chunk.js"
+  },
+  {
+    "revision": "fb0d19a6bf83d3c405ad",
+    "url": "/Text-Modifier/static/js/3.a80ba8e7.chunk.js"
+  },
+  {
+    "revision": "1316a5a852c3d3b53dba",
+    "url": "/Text-Modifier/static/js/main.167ad21a.chunk.js"
+  },
+  {
+    "revision": "45f608744540a277fe06",
+    "url": "/Text-Modifier/static/js/runtime~main.251f261e.js"
+  }
+]);
